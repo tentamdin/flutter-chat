6621003ad66462c05d0675056d89dad6c813be0a
tentamdin <tgtamdin3@gmail.com>
2024-07-26 17:28:21 +0530

    upload image to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
-import 'package:chat_app/screens/auth.dart';
-import 'package:chat_app/screens/chat.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'package:chat_app/screens/splash.dart';
+import 'package:chat_app/screens/chat.dart';
 import 'firebase_options.dart';
+import 'package:chat_app/screens/auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -30,6 +30,10 @@ class App extends StatelessWidget {
       home: StreamBuilder(
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SplashScreen();
+            }
+
             if (snapshot.hasData) {
               return const ChatScreen();
             }
